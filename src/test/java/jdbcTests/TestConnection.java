baa5ebf8c31bdbbc3f2aabac7910d4dd61b928b6
Elshat <z90602190@hotmail.com>
2022-07-24 08:22:34 +0300

    Day04:JDBC Intro

---
+++ src/test/java/jdbcTests/TestConnection.java
@@ -0,0 +1,30 @@
+package jdbcTests;
+
+import java.sql.*;
+
+public class TestConnection {
+
+    public static void main(String[] args) throws SQLException {
+
+        String dbUrl = "jdbc:oracle:thin:@44.202.63.224:1521:XE";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+
+        Connection connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM regions");
+
+        //next()--->move pointer to first row
+        resultSet.next();
+
+        //getting information with column name
+
+
+
+        // Close connections
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
+}
