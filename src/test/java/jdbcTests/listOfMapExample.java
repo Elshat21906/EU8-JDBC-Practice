d92d941d6fffc5ab273e8fb74a4e0b596ecebed4
Elshat <z90602190@hotmail.com>
2022-07-25 09:19:22 +0300

    Day05:JDBC

---
+++ src/test/java/jdbcTests/listOfMapExample.java
@@ -0,0 +1,18 @@
+package jdbcTests;
+
+import org.junit.jupiter.api.Test;
+
+public class listOfMapExample {
+
+    String dbUrl = "jdbc:oracle:thin:@44.202.63.224:1521:XE";
+    String dbUsername = "hr";
+    String dbPassword = "hr";
+
+    @Test
+    public void test1(){
+
+
+
+    }
+
+}
